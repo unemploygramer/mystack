062a4cd12cfe2062d550e93257e939eaa57ee038
Tyler <tystojack@gmail.com>
2024-06-29 22:02:33 -0500

    stuff

--- app/api/getCredits/[email]/route.ts
+++ app/api/getCredits/[email]/route.ts
@@ -3,7 +3,6 @@ import {connect} from "../../../../utils/config/dbConfig"
 import User from "../../../../models/User"
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-// export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
 export const GET: any = async (req: NextRequest, context: any) => {
   try {
     const {email} = await context.params;
@@ -13,14 +12,7 @@ export const GET: any = async (req: NextRequest, context: any) => {
     const fetchedUser = await User.find({email:email});
     console.log(fetchedUser[0].credits,"fetched user")
     const creds = fetchedUser[0].credits
-//  const FetchedScripts =  await Script.find({company: 'toyota'}, function (err, docs) { 
-//   if (err){ 
-//       console.log(err); 
-//   } 
-//   else{ 
-//       console.log("First function call : ", docs); 
-//   } 
-// }); 
+
 
 
 
