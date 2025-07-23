e22feb615b837f521c71ce50db4476bb223cbe7d
Tyler <tystojack@gmail.com>
2025-07-23 09:44:20 -0500

    rename

--- app/api/validateToken/[token]/route.js
+++ app/api/validateToken/[token]/route.js
@@ -3,28 +3,25 @@ import {connect} from "../../../../utils/config/dbConfig"
 import User from "../../../../models/User";
 import { NextResponse } from "next/server";
 
-//export async function GET(req) {
-export const GET: any = async (req: NextRequest, context: any) => {
-  try {
-        const {token} = await context.params;
 
-    console.log(token,"token!!!!!!!!!!!!!!!!!!!!")
+//export async function GET(req) {
+export async function GET(req, context) {
+  try {
+    const { token } = context.params;
+
+    console.log(token, 'token!!!!!!!!!!!!!!!!!!!!');
     await connect();
 
-    // Find user with the token
     const user = await User.findOne({ resetToken: token });
     if (!user) {
-      return NextResponse.json(
-        { valid: false },
-        { status: 400 }
-      );
+      return NextResponse.json({ valid: false }, { status: 400 });
     }
 
     return NextResponse.json({ valid: true }, { status: 200 });
   } catch (error) {
-  console.log(error,"error of validate token^^^^^^^^^^^^^^^^^^^^")
+    console.log(error, 'error of validate token^^^^^^^^^^^^^^^^^^^^');
     return NextResponse.json(
-      { message: "An error occurred while validating the token." },
+      { message: 'An error occurred while validating the token.' },
       { status: 500 }
     );
   }
